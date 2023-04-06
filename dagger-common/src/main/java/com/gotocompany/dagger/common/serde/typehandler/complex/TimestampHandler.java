4129788fa574ec5d3d2742621b96b52ac6875a1a
Sumit Aich <aich.1998@gmail.com>
2023-04-06 17:06:29 +0530

    refactor: rename transformFromProtoMap() to transformFromProtoUsingCache()

--- dagger-common/src/main/java/com/gotocompany/dagger/common/serde/typehandler/complex/TimestampHandler.java
+++ dagger-common/src/main/java/com/gotocompany/dagger/common/serde/typehandler/complex/TimestampHandler.java
@@ -110,8 +110,8 @@ public class TimestampHandler implements TypeHandler {
     }
 
     @Override
-    public Object transformFromProtoMap(Object field, FieldDescriptorCache map) {
-        return RowFactory.createRow((DynamicMessage) field, map);
+    public Object transformFromProtoUsingCache(Object field, FieldDescriptorCache cache) {
+        return RowFactory.createRow((DynamicMessage) field, cache);
     }
 
     @Override
