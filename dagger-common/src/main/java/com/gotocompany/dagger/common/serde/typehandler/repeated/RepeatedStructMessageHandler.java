b9543b3dae977575d82a50297e606c44b7f15dd5
Sumit Aich <aich.1998@gmail.com>
2023-04-06 17:16:23 +0530

    refactor: rename transformFromProtoMap() to transformFromProtoUsingCache()

--- dagger-common/src/main/java/com/gotocompany/dagger/common/serde/typehandler/repeated/RepeatedStructMessageHandler.java
+++ dagger-common/src/main/java/com/gotocompany/dagger/common/serde/typehandler/repeated/RepeatedStructMessageHandler.java
@@ -46,7 +46,7 @@ public class RepeatedStructMessageHandler implements TypeHandler {
     }
 
     @Override
-    public Object transformFromProtoMap(Object field, FieldDescriptorCache map) {
+    public Object transformFromProtoUsingCache(Object field, FieldDescriptorCache cache) {
         return null;
     }
 
