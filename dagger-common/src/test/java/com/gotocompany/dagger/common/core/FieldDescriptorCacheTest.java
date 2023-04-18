2e2c245ddf2e484d990f686ffb440067b9f98e5b
Sumit Aich <aich.1998@gmail.com>
2023-04-18 17:54:28 +0530

    feat: auto schema refresh

--- dagger-common/src/test/java/com/gotocompany/dagger/common/core/FieldDescriptorCacheTest.java
+++ dagger-common/src/test/java/com/gotocompany/dagger/common/core/FieldDescriptorCacheTest.java
@@ -1,19 +1,14 @@
 package com.gotocompany.dagger.common.core;
 
-import com.gotocompany.dagger.common.configuration.Configuration;
 import com.gotocompany.dagger.consumer.TestBookingLogMessage;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 
 import static org.junit.Assert.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class FieldDescriptorCacheTest {
 
-
-    @Mock
-    private Configuration configuration;
 
     @Before
     public void setup() {
@@ -22,25 +17,25 @@ public class FieldDescriptorCacheTest {
 
     @Test
     public void shouldReturnTrueIfFieldPresentInMap() {
-        FieldDescriptorCache fieldDescriptorCache = new FieldDescriptorCache(TestBookingLogMessage.getDescriptor(), true);
+        FieldDescriptorCache fieldDescriptorCache = new FieldDescriptorCache(TestBookingLogMessage.getDescriptor());
         assertTrue(fieldDescriptorCache.containsField("com.gotocompany.dagger.consumer.TestBookingLogMessage.order_number"));
     }
 
     @Test
     public void shouldReturnFalseIfFieldNotPresentInMap() {
-        FieldDescriptorCache fieldDescriptorCache = new FieldDescriptorCache(TestBookingLogMessage.getDescriptor(), true);
+        FieldDescriptorCache fieldDescriptorCache = new FieldDescriptorCache(TestBookingLogMessage.getDescriptor());
         assertFalse(fieldDescriptorCache.containsField("xyz"));
     }
 
     @Test
     public void shouldReturnOriginalFieldIndex() {
-        FieldDescriptorCache fieldDescriptorCache = new FieldDescriptorCache(TestBookingLogMessage.getDescriptor(), true);
+        FieldDescriptorCache fieldDescriptorCache = new FieldDescriptorCache(TestBookingLogMessage.getDescriptor());
         assertEquals(1, fieldDescriptorCache.getOriginalFieldIndex(TestBookingLogMessage.getDescriptor().findFieldByName("order_number")));
     }
 
     @Test
     public void shouldReturnOriginalFieldCount() {
-        FieldDescriptorCache fieldDescriptorCache = new FieldDescriptorCache(TestBookingLogMessage.getDescriptor(), true);
+        FieldDescriptorCache fieldDescriptorCache = new FieldDescriptorCache(TestBookingLogMessage.getDescriptor());
         assertEquals(49, fieldDescriptorCache.getOriginalFieldCount(TestBookingLogMessage.getDescriptor()));
     }
 }
