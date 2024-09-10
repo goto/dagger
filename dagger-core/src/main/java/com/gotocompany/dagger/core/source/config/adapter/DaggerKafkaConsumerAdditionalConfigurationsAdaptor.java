d2c981d8a88362b137ea2680eae75c924791587f
Eka Winata <eka.winata@gopay.co.id>
2024-09-10 14:39:58 +0700

    Added validations on source config

--- dagger-core/src/main/java/com/gotocompany/dagger/core/source/config/adapter/DaggerKafkaConsumerAdditionalConfigurationsAdaptor.java
+++ dagger-core/src/main/java/com/gotocompany/dagger/core/source/config/adapter/DaggerKafkaConsumerAdditionalConfigurationsAdaptor.java
@@ -6,9 +6,15 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class DaggerKafkaConsumerAdditionalConfigurationsAdaptor extends TypeAdapter<Map<String, String>> {
+
+    private static final String SOURCE_KAFKA_CONSUMER_CONFIG = "SOURCE_KAFKA_CONSUMER_CONFIG_(.*)";
+
     @Override
     public void write(JsonWriter jsonWriter, Map<String, String> stringStringMap) throws IOException {
         Gson gson = new Gson();
@@ -17,7 +23,16 @@ public class DaggerKafkaConsumerAdditionalConfigurationsAdaptor extends TypeAdap
 
     @Override
     public Map<String, String> read(JsonReader jsonReader) throws IOException {
+        Pattern pattern = Pattern.compile(SOURCE_KAFKA_CONSUMER_CONFIG, Pattern.CASE_INSENSITIVE);
         Gson gson = new Gson();
-        return gson.fromJson(jsonReader, Map.class);
+        Map<String,String> map = gson.fromJson(jsonReader, Map.class);
+        List<String> invalidProps = map.keySet().stream()
+                .filter(key -> !pattern.matcher(key).matches())
+                .collect(Collectors.toList());
+        if (!invalidProps.isEmpty()) {
+            throw new IllegalArgumentException("Invalid additional kafka consumer configuration properties found: " + invalidProps);
+        }
+        return map;
     }
+
 }
