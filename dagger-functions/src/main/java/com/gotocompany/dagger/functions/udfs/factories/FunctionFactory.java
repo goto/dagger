6b9ba0304bbb2b2bcc1de46aab12353df458bec6
rajuGT <raju.gt@gojek.com>
2024-10-23 02:10:19 +0530

    Dart Support for COS Service Provider

--- dagger-functions/src/main/java/com/gotocompany/dagger/functions/udfs/factories/FunctionFactory.java
+++ dagger-functions/src/main/java/com/gotocompany/dagger/functions/udfs/factories/FunctionFactory.java
@@ -4,6 +4,7 @@ import com.gotocompany.dagger.functions.common.Constants;
 import com.gotocompany.dagger.functions.udfs.scalar.dart.store.DartDataStore;
 import com.gotocompany.dagger.functions.udfs.scalar.dart.store.DartDataStoreClient;
 import com.gotocompany.dagger.functions.udfs.scalar.dart.store.DefaultDartDataStore;
+import com.gotocompany.dagger.functions.udfs.scalar.dart.store.cos.CosDartClient;
 import com.gotocompany.dagger.functions.udfs.scalar.dart.store.gcs.GcsDartClient;
 import com.gotocompany.dagger.functions.udfs.scalar.dart.store.oss.OssDartClient;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
@@ -155,6 +156,10 @@ public class FunctionFactory extends UdfFactory {
             case Constants.UDF_STORE_PROVIDER_OSS:
                 // TODO Check if OSS SDK supports projectID selection
                 dartDataStoreClient = new OssDartClient();
+                break;
+            case Constants.UDF_STORE_PROVIDER_COS:
+                // TODO Check if COS SDK supports projectID selection
+                dartDataStoreClient = new CosDartClient();
                 break;
             default:
                 throw new IllegalArgumentException("Unknown UDF Store Provider: " + udfStoreProvider);
