dc18103c0ae4b91ed27d95f9a1e1f37b328df3d6
rajuGT <rajugt.353@gmail.com>
2025-01-23 13:22:37 +0530

    Refactor Dart Feature to plug other object storage service providers (#44)
    * Add gradle tasks to minimal and dependencies to maven local
    
    * Add capability to dagger to read python udfs from Ali(oss) and Tencent(cosn) storage services
    Given the configuration provided correctly. Set the below environment
    variables accordingly to access the files stored in the respective
    bucket.
    
    Ali(oss)
    - OSS_ACCESS_KEY_ID
    - OSS_ACCESS_KEY_SECRET
    
    Tencent(cos)
    - COS_SECRET_ID
    - COS_SECRET_KEY
    - COS_REGION
    
    * OSS client endpoint should be configurable via ENV variable
    
    * COS filesystem high availability support
    If you need to use COS filesystem for the dagger, provide the cos
    bucket/key configuration in the state.backend.fs.checkpointdir,
    state.savepoints.dir, high-availability.storageDir to flinkdeployment
    manifest.
    
    If the filesystem protocol begins with cosn for the above
    configurations, dagger uses the below configurations provided in the
    flinkdeployment manifest file.
    
        fs.cosn.impl: org.apache.hadoop.fs.CosFileSystem
        fs.AbstractFileSystem.cosn.impl: org.apache.hadoop.fs.CosN
        fs.cosn.userinfo.secretId: <secretID>
        fs.cosn.userinfo.secretKey: <secretKey>
        fs.cosn.bucket.region: <region>
        fs.cosn.bucket.endpoint_suffix: <tencent-provided-prefix.xyz.com>
    
    * Fix checkstyle and made constants as static variables
    
    * Refactor Dart Feature to plug other object storage service providers
    
    * test checkstyle fix
    
    ---------
    
    Co-authored-by: Raju G T <rajugt@Raju-Gt.local>
    Co-authored-by: rajuGT <raju.gt@gojek.com>

--- dagger-functions/src/test/java/com/gotocompany/dagger/functions/udfs/scalar/DartContainsTest.java
+++ dagger-functions/src/test/java/com/gotocompany/dagger/functions/udfs/scalar/DartContainsTest.java
@@ -1,9 +1,10 @@
 package com.gotocompany.dagger.functions.udfs.scalar;
 
-
+import com.gotocompany.dagger.common.metrics.managers.GaugeStatsManager;
 import com.gotocompany.dagger.common.metrics.managers.MeterStatsManager;
 import com.gotocompany.dagger.functions.exceptions.TagDoesNotExistException;
-import com.gotocompany.dagger.functions.udfs.scalar.dart.store.gcs.GcsDataStore;
+import com.gotocompany.dagger.functions.udfs.scalar.dart.store.DartDataStore;
+import com.gotocompany.dagger.functions.udfs.scalar.dart.store.gcs.GcsDartDataStore;
 import com.gotocompany.dagger.functions.udfs.scalar.dart.types.SetCache;
 import org.apache.flink.metrics.Gauge;
 import org.apache.flink.metrics.MetricGroup;
@@ -14,13 +15,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static java.util.Collections.singleton;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class DartContainsTest {
-    private GcsDataStore dataStore;
+    private DartDataStore dataStore;
 
     @Mock
     private MetricGroup metricGroup;
@@ -31,173 +32,154 @@ public class DartContainsTest {
     @Mock
     private MeterStatsManager meterStatsManager;
 
+    @Mock
+    private GaugeStatsManager gaugeStatsManager;
+
+    // Subject
+    private DartContains dartContains;
+
     @Before
     public void setUp() {
         initMocks(this);
         when(functionContext.getMetricGroup()).thenReturn(metricGroup);
         when(metricGroup.addGroup("udf", "DartContains")).thenReturn(metricGroup);
         when(metricGroup.addGroup("DartContains")).thenReturn(metricGroup);
-        this.dataStore = mock(GcsDataStore.class);
-        when(dataStore.getMeterStatsManager()).thenReturn(meterStatsManager);
+        this.dataStore = mock(GcsDartDataStore.class);
+
+        dartContains = new DartContains(dataStore);
+
+        dartContains.setMeterStatsManager(meterStatsManager);
+        dartContains.setGaugeStatsManager(gaugeStatsManager);
     }
 
     @Test
     public void shouldReturnTrueWhenFieldContainsTheValue() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("someField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("someField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(true, dartContains.eval("someList", "someField", 0));
+        assertTrue(dartContains.eval("someList", "someField", 0));
     }
 
     @Test
     public void shouldReturnTrueWhenFieldContainsTheValueFromDifferentPaths() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("someField")));
-        when(dataStore.getSet("otherList")).thenReturn(new SetCache(singleton("otherField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("someField")));
+        when(dataStore.getSet("otherList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("otherField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(true, dartContains.eval("someList", "someField", 0));
-        assertEquals(true, dartContains.eval("otherList", "otherField", 0));
+        assertTrue(dartContains.eval("someList", "someField", 0));
+        assertTrue(dartContains.eval("otherList", "otherField", 0));
     }
 
     @Test
     public void shouldReturnFalseWhenFieldDoesNotContainsTheValue() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("someField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("someField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(false, dartContains.eval("someList", "otherField", 0));
+        assertFalse(dartContains.eval("someList", "otherField", 0));
     }
 
     @Test(expected = TagDoesNotExistException.class)
     public void shouldThrowErrorWhenFieldIsNotExist() {
-        when(dataStore.getSet("nonExistingList")).thenThrow(TagDoesNotExistException.class);
-
-        DartContains dartContains = new DartContains(dataStore);
+        when(dataStore.getSet("nonExistingList", meterStatsManager, gaugeStatsManager)).thenThrow(TagDoesNotExistException.class);
 
         dartContains.eval("nonExistingList", "someField", 0);
     }
 
     @Test
     public void shouldNotInvokeDataSourceWhenInvokedAgainWithinRefreshRate() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("someField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("someField")));
 
-        DartContains dartContains = new DartContains(dataStore);
         dartContains.eval("someList", "someField", 1);
         dartContains.eval("someList", "otherField", 1);
 
-        verify(dataStore, times(1)).getSet("someList");
+        verify(dataStore, times(1)).getSet("someList", meterStatsManager, gaugeStatsManager);
     }
 
     @Test
     public void shouldInvokeDataSourceWhenExceededRefreshRate() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("someField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("someField")));
 
-        DartContains dartContains = new DartContains(dataStore);
         dartContains.eval("someList", "someField", -1);
         dartContains.eval("someList", "otherField", -1);
 
-        verify(dataStore, times(2)).getSet("someList");
+        verify(dataStore, times(2)).getSet("someList", meterStatsManager, gaugeStatsManager);
     }
 
     @Test
     public void shouldReturnTrueWhenFieldContainsTheValueInMiddleWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("prefixsomeField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("prefixsomeField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(true, dartContains.eval("someList", "a sentence with prefixsomeField and an end", ".*%s.*"));
+        assertTrue(dartContains.eval("someList", "a sentence with prefixsomeField and an end", ".*%s.*"));
     }
 
     @Test
     public void shouldReturnFalseWhenTagContainsSpaceAndFieldDoesNotWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("prefixsomeField ")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("prefixsomeField ")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(false, dartContains.eval("someList", "a sentence with prefixsomeFieldsuffix and an end", ".*%s.*"));
+        assertFalse(dartContains.eval("someList", "a sentence with prefixsomeFieldsuffix and an end", ".*%s.*"));
     }
 
     @Test
     public void shouldReturnTrueWhenFieldContainsTheValueAtEndWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("prefixsomeField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("prefixsomeField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(true, dartContains.eval("someList", "a sentence that ends with prefixsomeField", ".*%s"));
+        assertTrue(dartContains.eval("someList", "a sentence that ends with prefixsomeField", ".*%s"));
     }
 
     @Test
     public void shouldReturnTrueWhenFieldContainsTheValueAtBeginningWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("prefixsomeField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("prefixsomeField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(true, dartContains.eval("someList", "prefixsomeField is the start of this sentence", "%s.*"));
+        assertTrue(dartContains.eval("someList", "prefixsomeField is the start of this sentence", "%s.*"));
     }
 
     @Test
     public void shouldReturnTrueWhenFieldContainsEntireValueWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("prefixsomeField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("prefixsomeField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(true, dartContains.eval("someList", "prefixsomeField", "%s"));
+        assertTrue(dartContains.eval("someList", "prefixsomeField", "%s"));
     }
 
     @Test
     public void shouldReturnFalseWhenFieldContainsValueNotInSameCaseWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("prefixsomeField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("prefixsomeField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(false, dartContains.eval("someList", "preFixSomEfield", ".*%s.*"));
+        assertFalse(dartContains.eval("someList", "preFixSomEfield", ".*%s.*"));
     }
 
     @Test
     public void shouldReturnFalseWhenFieldDoesNotContainsTheValueWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("someField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("someField")));
 
-        DartContains dartContains = new DartContains(dataStore);
-
-        assertEquals(false, dartContains.eval("someList", "other", ".*%s.*"));
+        assertFalse(dartContains.eval("someList", "other", ".*%s.*"));
     }
 
     @Test(expected = TagDoesNotExistException.class)
     public void shouldThrowErrorWhenFieldIsNotExistWithARegex() {
-        when(dataStore.getSet("nonExistingList")).thenThrow(TagDoesNotExistException.class);
-
-        DartContains dartContains = new DartContains(dataStore);
+        when(dataStore.getSet("nonExistingList", meterStatsManager, gaugeStatsManager)).thenThrow(TagDoesNotExistException.class);
 
         dartContains.eval("nonExistingList", "someField", ".*%s.*");
     }
 
     @Test
     public void shouldNotInvokeDataSourceWhenInvokedAgainWithinRefreshRateWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("someField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("someField")));
 
-        DartContains dartContains = new DartContains(dataStore);
         dartContains.eval("someList", "someField", ".*%s.*", 1);
         dartContains.eval("someList", "otherField", ".*%s.*", 1);
 
-        verify(dataStore, times(1)).getSet("someList");
+        verify(dataStore, times(1)).getSet("someList", meterStatsManager, gaugeStatsManager);
     }
 
     @Test
     public void shouldInvokeDataSourceWhenExceededRefreshRateWithARegex() {
-        when(dataStore.getSet("someList")).thenReturn(new SetCache(singleton("someField")));
+        when(dataStore.getSet("someList", meterStatsManager, gaugeStatsManager)).thenReturn(new SetCache(singleton("someField")));
 
-        DartContains dartContains = new DartContains(dataStore);
         dartContains.eval("someList", "someField", ".*%s.*", -1);
         dartContains.eval("someList", "otherField", ".*%s.*", -1);
 
-        verify(dataStore, times(2)).getSet("someList");
+        verify(dataStore, times(2)).getSet("someList", meterStatsManager, gaugeStatsManager);
     }
 
     @Test
     public void shouldRegisterGauge() throws Exception {
-        DartContains dartContains = new DartContains(dataStore);
         dartContains.open(functionContext);
         verify(metricGroup, Mockito.times(1)).gauge(any(String.class), any(Gauge.class));
     }
