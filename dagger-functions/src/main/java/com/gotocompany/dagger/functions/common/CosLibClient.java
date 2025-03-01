ca24ea640a30196e6c173d4c154e1e9ad5d4ff83
rajuGT <rajugt.353@gmail.com>
2025-03-01 15:58:19 +0530

    chore: Add toggle switch for tencent TKE  oidc credential provider, fallback to env credential provider (#56)
    Co-authored-by: rajuGT <raju.gt@gojek.com>

--- dagger-functions/src/main/java/com/gotocompany/dagger/functions/common/CosLibClient.java
+++ dagger-functions/src/main/java/com/gotocompany/dagger/functions/common/CosLibClient.java
@@ -17,18 +17,29 @@ public class CosLibClient {
     @Getter
     private static CosLibClient instance = new CosLibClient();
 
+    private static final String ENV_COS_SECRET_ID = "COS_SECRET_ID";
+    private static final String ENV_COS_SECRET_KEY = "COS_SECRET_KEY";
     private static final String ENV_COS_REGION = "COS_REGION";
+    private static final String ENV_ENABLE_TKE_OIDC_PROVIDER = "ENABLE_TKE_OIDC_PROVIDER";
 
     public COSClient get() {
+        String secretId, secretKey;
         String region = System.getenv(ENV_COS_REGION);
-        Credential credentials;
-        try {
-            credentials = new OIDCRoleArnProvider().getCredentials();
-        } catch (TencentCloudSDKException e) {
-            throw new RuntimeException("failed to initiate oidc credential provider", e);
+
+        if (System.getenv(ENV_ENABLE_TKE_OIDC_PROVIDER).trim().equalsIgnoreCase("true")) {
+            try {
+                Credential credentials = new OIDCRoleArnProvider().getCredentials();
+                secretId = credentials.getSecretId();
+                secretKey = credentials.getSecretKey();
+            } catch (TencentCloudSDKException e) {
+                throw new RuntimeException("failed to initiate oidc credential provider", e);
+            }
+        } else {
+            secretId = System.getenv(ENV_COS_SECRET_ID);
+            secretKey = System.getenv(ENV_COS_SECRET_KEY);
         }
 
-        COSCredentials cosCredentials = new BasicCOSCredentials(credentials.getSecretId(), credentials.getSecretKey());
+        COSCredentials cosCredentials = new BasicCOSCredentials(secretId, secretKey);
         ClientConfig clientConfig = new ClientConfig(new Region(region));
         return new COSClient(cosCredentials, clientConfig);
     }
