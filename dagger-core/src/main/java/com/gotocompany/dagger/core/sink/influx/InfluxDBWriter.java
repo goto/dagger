9ca07c2a6503ffe902dd184688e1d2cf6e1da654
rajuGT <raju.gt@gojek.com>
2025-10-07 01:27:10 +0530

    InfluxDBWriter can now use row.getFieldNames() implementation if SINK_INFLUX_WITH_ROW_NAMES_WRITER is set to true

--- dagger-core/src/main/java/com/gotocompany/dagger/core/sink/influx/InfluxDBWriter.java
+++ dagger-core/src/main/java/com/gotocompany/dagger/core/sink/influx/InfluxDBWriter.java
@@ -7,6 +7,7 @@ import org.apache.flink.types.Row;
 
 import com.google.common.base.Strings;
 import com.gotocompany.dagger.common.configuration.Configuration;
+import org.apache.flink.util.Preconditions;
 import org.influxdb.InfluxDB;
 import org.influxdb.dto.Point;
 import org.influxdb.dto.Point.Builder;
@@ -21,6 +22,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class InfluxDBWriter implements SinkWriter<Row, Void, Void> {
@@ -32,11 +34,13 @@ public class InfluxDBWriter implements SinkWriter<Row, Void, Void> {
     private String[] columnNames;
     private ErrorHandler errorHandler;
     private ErrorReporter errorReporter;
+    private boolean useRowFieldNames;
 
     public InfluxDBWriter(Configuration configuration, InfluxDB influxDB, String[] columnNames, ErrorHandler errorHandler, ErrorReporter errorReporter) {
         databaseName = configuration.getString(Constants.SINK_INFLUX_DB_NAME_KEY, Constants.SINK_INFLUX_DB_NAME_DEFAULT);
         retentionPolicy = configuration.getString(Constants.SINK_INFLUX_RETENTION_POLICY_KEY, Constants.SINK_INFLUX_RETENTION_POLICY_DEFAULT);
         measurementName = configuration.getString(Constants.SINK_INFLUX_MEASUREMENT_NAME_KEY, Constants.SINK_INFLUX_MEASUREMENT_NAME_DEFAULT);
+        useRowFieldNames = configuration.getBoolean(Constants.SINK_INFLUX_USING_ROW_FIELD_NAMES_KEY, Constants.SINK_INFLUX_USING_ROW_FIELD_NAMES_DEFAULT);
         this.influxDB = influxDB;
         this.columnNames = columnNames;
         this.errorHandler = errorHandler;
@@ -47,8 +51,27 @@ public class InfluxDBWriter implements SinkWriter<Row, Void, Void> {
     public void write(Row row, Context context) throws IOException, InterruptedException {
         LOGGER.info("row to influx: " + row);
 
-        Builder pointBuilder = Point.measurement(measurementName);
+        Builder pointBuilder;
         Map<String, Object> fields = new HashMap<>();
+        if (useRowFieldNames) {
+            pointBuilder = writeUsingRowFieldNames(row, fields);
+        } else {
+            pointBuilder = writeUsingColumnNames(row, fields);
+        }
+
+        addErrorMetricsAndThrow();
+
+        try {
+            influxDB.write(databaseName, retentionPolicy, pointBuilder.fields(fields).build());
+        } catch (Exception exception) {
+            errorReporter.reportFatalException(exception);
+            throw exception;
+        }
+    }
+
+    private Builder writeUsingColumnNames(Row row, Map<String, Object> fields) {
+        Builder pointBuilder = Point.measurement(measurementName);
+
         for (int i = 0; i < columnNames.length; i++) {
             String columnName = columnNames[i];
             if (columnName.equals("window_timestamp")) {
@@ -65,15 +88,31 @@ public class InfluxDBWriter implements SinkWriter<Row, Void, Void> {
                 }
             }
         }
+        return pointBuilder;
+    }
 
-        addErrorMetricsAndThrow();
+    private Builder writeUsingRowFieldNames(Row row, Map<String, Object> fields) {
+        Builder pointBuilder = Point.measurement(measurementName);
 
-        try {
-            influxDB.write(databaseName, retentionPolicy, pointBuilder.fields(fields).build());
-        } catch (Exception exception) {
-            errorReporter.reportFatalException(exception);
-            throw exception;
+        Set<String> fieldNames = row.getFieldNames(false);
+        Preconditions.checkNotNull(fieldNames, "Error! in writeUsingRowFieldNames, getFieldNames() returned null");
+
+        for (String fieldName : fieldNames) {
+            if (fieldName.equals("window_timestamp")) {
+                LocalDateTime timeField = (LocalDateTime) row.getField(fieldName);
+                ZonedDateTime zonedDateTime = timeField.atZone(ZoneOffset.UTC);
+                pointBuilder.time(zonedDateTime.toInstant().toEpochMilli(), TimeUnit.MILLISECONDS);
+            } else if (fieldName.startsWith("tag_")) {
+                pointBuilder.tag(fieldName, String.valueOf(row.getField(fieldName)));
+            } else if (fieldName.startsWith("label_")) {
+                pointBuilder.tag(fieldName.substring("label_".length()), String.valueOf(row.getField(fieldName)));
+            } else {
+                if (!(Strings.isNullOrEmpty(fieldName) || row.getField(fieldName) == null)) {
+                    fields.put(fieldName, row.getField(fieldName));
+                }
+            }
         }
+        return pointBuilder;
     }
 
     @Override
